c7bbd7e0e654846db013656f2c141afe6a1fbf79
Max Tsao <mt9304@gmail.com>
2023-12-05 17:32:32 -0800

    Finished.

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
 
 window.addEventListener("load", function () {
     //Overlay fade and zoom on first loading page. 
-    /*
-    const overlay = document.createElement("div");
-    overlay.classList.add("overlay");
-    overlay.style.opacity = "0.9"; // Set initial opacity to 80%
-
-    overlay.textContent = "Loading...";
-    overlay.style.textAlign ="center";
-    overlay.style.paddingTop ="20%";
-    overlay.style.color ="white";
-    overlay.style.position ="fixed";
-    document.body.appendChild(overlay);
-    */
-
     const overlay = document.getElementsByClassName("overlay")[0];
   
     const homeSection = document.getElementById("home");
@@ -21,29 +8,13 @@ window.addEventListener("load", function () {
   
     setTimeout(() => {
       overlay.style.opacity = "0";
-      overlay.style.transition = "opacity 3s"; // Changed duration to 1 second
+      overlay.style.transition = "opacity 3s";
     }, 0); // Schedule the overlay transition to start immediately
   
     setTimeout(() => {
       overlay.style.display = "none";
-    }, 1000); // Changed the delay to 1000ms (1 second) for hiding the overlay
-  });
-
-/*
-document.addEventListener("DOMContentLoaded", function () {
-    const overlay = document.createElement("div");
-    overlay.classList.add("overlay");
-    document.body.appendChild(overlay);
-  
-    setTimeout(() => {
-      overlay.style.opacity = "0";
-      overlay.style.transition = "opacity 2s";
-      setTimeout(() => {
-        overlay.style.display = "none";
-      }, 2000);
     }, 1000);
   });
-*/
 
   document.addEventListener("DOMContentLoaded", function () {
     const booksSection = document.getElementById("books");
